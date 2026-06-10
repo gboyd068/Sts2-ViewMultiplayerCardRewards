7bf0bf0e239f0cf8c18eb04abdc7cb6dd935de64
Greg Boyd <gboyd068@gmail.com>
2026-06-10 18:13:19 +0100

    refactor display logic into CardRewardViewerPatch

--- ViewMultiplayerCardRewardsCode/ViewPlayerCardRewards.cs
+++ ViewMultiplayerCardRewardsCode/ViewPlayerCardRewards.cs
@@ -1,12 +1,19 @@
 using Godot;
 using HarmonyLib;
 using MegaCrit.Sts2.addons.mega_text;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes;
 using MegaCrit.Sts2.Core.Nodes.Multiplayer;
+using MegaCrit.Sts2.Core.Nodes.Screens.RunHistoryScreen;
 
 
 [HarmonyPatch]
 internal static class CardRewardViewerPatch
 {
+    
+    private static List<List<CardModel>> GroupedCards;
+    
     [HarmonyPatch(typeof(NMultiplayerPlayerExpandedState), "_Ready")]
     [HarmonyPostfix]
     private static void AfterReady(NMultiplayerPlayerExpandedState __instance)
@@ -22,7 +29,46 @@ internal static class CardRewardViewerPatch
         rewardsLabel.Hide();
         __instance.AddChild(rewardsLabel);
         __instance.CallDeferred(Node.MethodName.AddChild, cardRewardContainer);
-        PlayerCardRewardGroupsDisplay.groupedCards = PlayerCardRewardGroupsDisplay.GetGroupedCardRewards(__instance);
-        PlayerCardRewardGroupsDisplay.Populate(cardRewardContainer, rewardsLabel);
+        GroupedCards = GetGroupedCardRewards(__instance);
+        Populate(cardRewardContainer, rewardsLabel);
+    }
+
+    private static void Populate(Control node, MegaRichTextLabel label)
+    {
+        foreach (var group in GroupedCards)
+        {
+            if (group.Count == 0)
+                continue;
+            label.Show();
+            GridContainer groupContainer = new GridContainer();
+            if (group.Count > 5)
+                groupContainer.Columns = 2;
+            node.AddChild(groupContainer);
+            foreach (var card in group)
+            {
+                var entry = NDeckHistoryEntry.Create(card, 1);
+                groupContainer.CallDeferred(Node.MethodName.AddChild, entry);
+                long num = (long) entry.Connect(NDeckHistoryEntry.SignalName.Clicked, Callable.From<NDeckHistoryEntry>(new Action<NDeckHistoryEntry>(ShowEntry)));
+            }
+        }
+    }
+
+    private static void ShowEntry(NDeckHistoryEntry entry)
+    {
+        var allCards = GroupedCards
+            .SelectMany(group => group)
+            .ToList();
+        NGame.Instance?.GetInspectCardScreen().Open(allCards, allCards.IndexOf(entry.Card));
+    }
+    
+    private static List<List<CardModel>> GetGroupedCardRewards(
+        NMultiplayerPlayerExpandedState state)
+    {
+        ulong netId = Traverse.Create(state).Field("_player").GetValue<Player>().NetId;
+        if (CardRewardsMap.Instance.Map.ContainsKey(netId))
+        {
+            return CardRewardsMap.Instance.Map[netId];
+        } 
+        return [];
     }
 }
